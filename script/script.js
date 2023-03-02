812ba7ff3350e4e88fbd20aea900b6c9def3b0f9
Max <maximesin21@gmail.com>
2023-03-02 15:02:07 +0700

    Hotfix: button disabled fixed

--- script/script.js
+++ script/script.js
@@ -170,8 +170,10 @@ const toggleButtonState = (inputList, buttonElement) => {
   console.log(hasInvalidInput(inputList));
   if (hasInvalidInput(inputList)) {
     buttonElement.classList.add('popup__button_inactive');
+    buttonElement.disabled = true;
   } else {
     buttonElement.classList.remove('popup__button_inactive');
+    buttonElement.disabled = false;
   }
 };
 
